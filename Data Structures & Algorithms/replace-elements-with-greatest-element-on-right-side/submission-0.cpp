c34c82d1aa220c8670c104de61f59a123c5e6d7e
TheWanderer123 <tranngocphuc31102007@gmail.com>
2026-07-14 19:11:52 +0700

    Add: replace-elements-with-greatest-element-on-right-side - submission-0

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int maxright=-1;
+        for (int i=arr.size()-1;i>=0;--i){
+            int current=arr[i];
+            arr[i]=maxright;
+            maxright=max(maxright,current);// đè xong mới cập nhật maxright
+        }
+        return arr;
+    }
+};
