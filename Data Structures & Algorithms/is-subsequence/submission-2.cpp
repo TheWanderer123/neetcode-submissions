12766090cc791e72bde4f09666179f85b5b4e882
TheWanderer123 <tranngocphuc31102007@gmail.com>
2026-07-14 19:44:47 +0700

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sp=0;
+        int tp=0;
+        while (sp<s.length()&&tp<t.length()){
+            if(s[sp]==t[tp]) ++sp; 
+            ++tp;
+        }
+        if (sp==s.length()) return true;
+        return false;
+    }
+};
