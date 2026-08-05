be518383af67d3a7dcbeee519a7d34036dc8d750
TheWanderer123 <tranngocphuc31102007@gmail.com>
2026-08-06 00:02:03 +0700

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; ++i) {
+            unordered_set<char>row_set;
+            unordered_set<char>col_set;
+            unordered_set<char>box_set;
+
+            int start_row = (i/3)*3; //for box_set
+            int start_col = (i%3)*3; //for box_set
+
+            for (int j=0;j<9;++j) {
+                if (board[i][j]!='.') {
+                    if (row_set.count(board[i][j])) return false;
+                    row_set.insert(board[i][j]);
+                }
+
+                if (board[j][i]!='.') {
+                    if (col_set.count(board[j][i])) return false;
+                    col_set.insert(board[j][i]);
+                }
+                // if its r then the row increases as j increase 3 scanned the box row if its c then it runs in loop 0,1,2 
+                int r = start_row+j/3;
+                int c = start_col+j%3;
+                if (board[r][c]!='.') {
+                    if (box_set.count(board[r][c])) return false;
+                    box_set.insert(board[r][c]);
+                }
+            }
+        }    
+        return true;
+        }    
+};
